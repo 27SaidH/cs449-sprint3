f059b12ccbfdde6718e4a54a5346d66332e894eb
27SaidH <157550542+27SaidH@users.noreply.github.com>
2026-05-06 16:27:45 -0500

    Update ManualSolitaireGame.cs

--- Solitaire.Core/Core/ManualSolitaireGame.cs
+++ Solitaire.Core/Core/ManualSolitaireGame.cs
@@ -1,6 +1,6 @@
-using cs449sprint2.Models;
+using cs449sprint3.Models;
 
-namespace cs449sprint2.Core
+namespace cs449sprint3.Core
 {
     public class ManualSolitaireGame : SolitaireGameBase
     {
@@ -9,7 +9,8 @@ namespace cs449sprint2.Core
         public void RandomizeBoard()
         {
             if (Board == null)
-                throw new InvalidOperationException("Start a game before randomizing the board.");
+                throw new InvalidOperationException(
+                    "Start a game before randomizing.");
 
             int maxAttempts = 50;
 
@@ -17,47 +18,53 @@ namespace cs449sprint2.Core
             {
                 RandomizePlayableCells();
 
-                if (Board.CountPegs() > 1 && GetValidMoves().Count > 0)
+                if (Board.CountPegs() > 1 &&
+                    GetValidMoves().Count > 0)
+                {
                     return;
+                }
             }
         }
 
         private void RandomizePlayableCells()
         {
-            var playablePositions = new List<(int Row, int Col)>();
+            var playablePositions =
+                new List<(int Row, int Col)>();
 
             for (int r = 0; r < Board.Size; r++)
             {
                 for (int c = 0; c < Board.Size; c++)
                 {
                     if (Board.IsPlayablePosition(r, c))
+                    {
                         playablePositions.Add((r, c));
+                    }
                 }
             }
 
-            if (playablePositions.Count == 0)
-                return;
-
             foreach (var pos in playablePositions)
             {
-                Board.SetCell(pos.Row, pos.Col, _random.Next(2) == 0 ? CellState.Peg : CellState.Empty);
-            }
-
-            if (Board.CountPegs() == playablePositions.Count)
-            {
-                var emptySpot = playablePositions[_random.Next(playablePositions.Count)];
-                Board.SetCell(emptySpot.Row, emptySpot.Col, CellState.Empty);
+                Board.SetCell(
+                    pos.Row,
+                    pos.Col,
+                    _random.Next(2) == 0
+                        ? CellState.Peg
+                        : CellState.Empty);
             }
 
             if (Board.CountPegs() < 2)
             {
                 int needed = 2 - Board.CountPegs();
+
                 while (needed > 0)
                 {
-                    var pegSpot = playablePositions[_random.Next(playablePositions.Count)];
-                    if (Board.GetCell(pegSpot.Row, pegSpot.Col) != CellState.Peg)
+                    var pos =
+                        playablePositions[
+                            _random.Next(playablePositions.Count)];
+
+                    if (Board.GetCell(pos.Row, pos.Col) != CellState.Peg)
                     {
-                        Board.SetCell(pegSpot.Row, pegSpot.Col, CellState.Peg);
+                        Board.SetCell(pos.Row, pos.Col, CellState.Peg);
                         needed--;
                     }
                 }
